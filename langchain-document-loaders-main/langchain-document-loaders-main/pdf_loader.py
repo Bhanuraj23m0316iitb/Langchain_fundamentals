06b5cec48c648686bb8576dd2b43bd4800458a91
Bhanuraj Badal <badalbhanuraj@gmail.com>
2025-10-27 12:10:22 +0530

    Add docstring to pdf_loader.py
    Added a docstring explaining the functionality of the PDF loader.

--- langchain-document-loaders-main/langchain-document-loaders-main/pdf_loader.py
+++ langchain-document-loaders-main/langchain-document-loaders-main/pdf_loader.py
@@ -7,4 +7,9 @@ docs = loader.load()
 print(len(docs))
 
 print(docs[0].page_content)
+
 print(docs[1].metadata)
+
+""" pypdf loader load page by page for each page of pdf you will get one document object that will content metadata and page content
+for example if you have pdf which has 25 pages so you will get list of 25 document object with each object containing metdata and page content
+""" 
